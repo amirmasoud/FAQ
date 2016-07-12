50da1fbfa5dd494b45d04533a09985a35e8efb72
AmirMasoud Sheidayi <AmirMasoud Sheidayi>
2016-07-12 11:57:39 +0430

    add questions migration

---
+++ database/migrations/2016_07_12_072404_create_questions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->index();
+            $table->text('text');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->unsignedInteger('answer')->default(0);
+            $table->foreign('answer')
+                  ->references('id')->on('questions')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
