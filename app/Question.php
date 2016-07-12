e547ce18f59f3b39e48e3560d10d69076b5c076d
AmirMasoud Sheidayi <AmirMasoud Sheidayi>
2016-07-12 11:59:39 +0430

    add Question Model

---
+++ app/Question.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+	/**
+	 * variable that can be mass assigned.
+     * 
+	 * @var array
+	 */
+	protected $fillable = [
+		'title',
+        'text',
+	];
+
+	/**
+	 * A question is owned by a user.
+	 * 
+	 * @return belongTo
+	 */
+	public function user()
+	{
+		return $this->belongsTo('App\User');
+	}
+}
